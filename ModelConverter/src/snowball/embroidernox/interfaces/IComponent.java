ae00ce76551c6a9a39979983e5bd2a12f463cf4e
PamSnowball <raphael_nerd12@hotmail.com>
2021-03-03 18:59:11 -0300

    03/03/2021

--- ModelConverter/src/snowball/embroidernox/interfaces/IComponent.java
+++
@@ -1,7 +0,0 @@
-package snowball.embroidernox.interfaces;
-
-import java.util.List;
-
-public interface IComponent {
-	void load(List<String> loader);
-}
--- ModelConverter/src/snowball/embroidernox/interfaces/IDeath.java
+++
@@ -1,7 +0,0 @@
-package snowball.embroidernox.interfaces;
-
-import java.util.List;
-
-public interface IDeath {
-	void death(List<String> death);
-}
